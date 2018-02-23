633e205bb7a2e15561ecf27d3dbcfb3584648808
Andrew <andrew.ramell@gmail.com>
2018-02-23 17:32:56 -0500

    added datatable showing username steps and heartrate

--- src/app/datatable/datatable.component.ts
+++ src/app/datatable/datatable.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { WatchService } from '../watch.service';
+import { Observable } from 'rxjs/Observable';
+import { DataSource} from '@angular/cdk/collections'
+import {User } from '../models/user.model'
 
 @Component({
   selector: 'datatable',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatatableComponent implements OnInit {
 
-  constructor() { }
+  dataSource = new UserDataSource(this.watchService)
+  displayedColumns = ['username', 'steps', 'heartrate']
+
+  constructor(private watchService: WatchService) { }
 
   ngOnInit() {
   }
 
+}
+export class UserDataSource extends DataSource<any>{
+  constructor(private watchService: WatchService){
+    super()
+  }
+  connect(): Observable<User[]>{
+    return this.watchService.getUser()
+  }
+  disconnect(){}
 }
